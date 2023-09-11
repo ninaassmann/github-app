403d80c192e999fba7f987d4c0a6ca175416e5e8
Nina Aßmann <hallo@ninaassmann.de>
2023-09-11 14:38:50 +0200

    add general styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 export default function HomePage() {
   const [username, setUsername] = useState();
@@ -8,21 +9,45 @@ export default function HomePage() {
 
     const inputValue = event.target.username.value;
     setUsername(inputValue);
+    event.target.reset();
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          placeholder="Enter a valid username"
-          id="username"
-          name="username"
-        />
-        <button>Check User</button>
-      </form>
-      <p>{username}</p>
-    </div>
+    <>
+      <main>
+        <header>
+          <h1>GitHub App</h1>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Enter a valid username</label>
+          <Input
+            type="text"
+            placeholder="e.g. ninaassmann"
+            id="username"
+            name="username"
+          />
+          <Button>Check User</Button>
+        </form>
+        <section>
+          <p>{username}</p>
+        </section>
+      </main>
+    </>
   );
 }
+
+const Input = styled.input`
+  padding: 1rem;
+  border: none;
+  border-radius: 0.5rem;
+`;
+
+const Button = styled.button`
+  padding: 1rem;
+  border: none;
+  border-radius: 0.5rem;
+  background: #188fa7;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`;
--- styles.js
+++ styles.js
@@ -1,14 +1,51 @@
 import { createGlobalStyle } from "styled-components";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default createGlobalStyle`
   *,
   *::before,
   *::after {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
 
   body {
     margin: 0;
-    font-family: system-ui;
+    font-family: ${inter.style.fontFamily};
+    background: #E2DBBE;
+    padding: 4rem 0;
+  }
+
+  header {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  main {
+    width: min(calc(100vw - 4rem), 600px);
+    margin-inline: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  section {
+    background: #769FB6;
+    color: #fff;
+    padding: 1rem;
+    border-radius: 0.5rem;
   }
 `;
